336bc10e98aea878850c990be8a564de9bd951ef
Yinuo Guo <yg202@duke.edu>
2024-12-01 22:05:05 +0800

    related_sections_view

--- screens/misc.py
+++ screens/misc.py
@@ -263,40 +263,12 @@ class ClassSearchScreen(Screen):
 
     def get_related_sections(self, course_id, term, session, year):
         query = """
-        SELECT 
-            s.id AS section_id,
-            s.type AS section_type,
-            c.id AS course_id,
-            c.name AS course_name,
-            c.dept_name,
-            c.credits,
-            s.term,
-            s.session,
-            s.year,
-            cl.building_name,
-            cl.room_name,
-            GROUP_CONCAT(
-                DISTINCT CONCAT(i.first_name, ' ', i.last_name)
-                ORDER BY i.first_name, i.last_name SEPARATOR '/'
-            ) AS instructors,
-            GROUP_CONCAT(
-                DISTINCT CONCAT(st.day, ' ', DATE_FORMAT(ts.start_time, '%H:%i'), '-', DATE_FORMAT(ts.end_time, '%H:%i'))
-                ORDER BY ts.start_time SEPARATOR ', '
-            ) AS course_schedule
-        FROM section s
-        JOIN course c ON s.course_id = c.id AND s.type = c.type
-        JOIN teaches t ON s.id = t.section_id
-        JOIN instructor_master i ON t.instructor_id = i.id
-        JOIN classroom cl ON s.building_name = cl.building_name AND s.room_name = cl.room_name
-        JOIN section_time st ON s.id = st.section_id
-        JOIN time_slot ts ON st.time_slot_id = ts.id
-        WHERE c.id = %s AND s.term = %s AND s.session = %s AND s.year = %s
-        GROUP BY 
-            s.id, s.type, c.id, c.name, c.dept_name, c.credits, 
-            s.term, s.session, s.year, cl.building_name, cl.room_name
-        ORDER BY s.type, s.id
+        SELECT *
+        FROM related_sections_view
+        WHERE course_id = %s AND term = %s AND session = %s AND year = %s
         """
         return self.session.db_connection.execute_query(query, (course_id, term, session, year))
+
 
     def select_related_sections(self, related_sections):
         selected_sections = {}
